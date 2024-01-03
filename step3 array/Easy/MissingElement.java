b5c1cc79e9c9b1897c6029bf0dfe36066e1970cf
Chetan-B-B <chetanchetu9740@gmail.com>
2024-01-03 14:26:56 +0530

    3-01-24

--- step3 array/Easy/MissingElement.java
+++ step3 array/Easy/MissingElement.java
@@ -5,5 +5,30 @@
 // Result: 3
 // Explanation: In the given array, number 3 is missing. So, 3 is the answer.
 public class MissingElement {
+    static void missingElement(int[] arr) {
+        // find sum of total numbers n*(n +1)/2
+        int len = arr.length;
+        // len += 1;
+        int total = (len * (len + 1)) / 2;
+        int givenSum = 0;
 
+        for (int i : arr) {
+            givenSum += i;
+        }
+        System.out.println(total - givenSum);
+    }
+
+    // https://leetcode.com/problems/missing-number/description/
+    static void missingElementWithZero(int[] arr) {
+
+    }
+
+    public static void main(String[] args) {
+        // asuming no zero's here
+        // len is size of array with the missing one element
+        missingElement(new int[] { 0, 1 });// 2
+        // note the diff , zero
+        missingElementWithZero(new int[] { 0, 1, 3 });// 2
+        missingElementWithZero(new int[] { 9, 6, 4, 2, 3, 5, 7, 0, 1 });// 8
+    }
 }
