310c6102655b9f82c1199f8b0e2f9d25ce5402d6
Chetan-B-B <chetanchetu9740@gmail.com>
2024-01-04 10:41:04 +0530

    4-1-2023

---
+++ step3 array/Easy/MaxConsecutiveCount.java
@@ -0,0 +1,23 @@
+
+public class MaxConsecutiveCount {
+
+    public static void main(String[] args) {
+        maxConsecutiveCount(new int[] { 1, 0, 1, 1, 0, 1, 1, 1, 1 });
+    }
+
+    private static void maxConsecutiveCount(int[] is) {
+        // begin with assuming we have no 1's
+        int count = 0;
+        int prevCount = 0;
+        for (int i : is) {
+            if (i == 1) {
+                count++;
+            } else {
+                prevCount = Math.max(count, prevCount);
+                count = 0;
+            }
+        }
+        System.out.println(prevCount + " " + count);
+        System.out.println(prevCount > count ? prevCount : count);
+    }
+}
