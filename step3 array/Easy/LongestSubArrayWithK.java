c6005443ecea50fac7fde7c8ffa3b2a0e718a5a3
Chetan-B-B <chetanchetu9740@gmail.com>
2024-02-15 08:24:24 +0530

    optimal code

--- step3 array/Easy/LongestSubArrayWithK.java
+++ step3 array/Easy/LongestSubArrayWithK.java
@@ -1,20 +1,30 @@
 // https://takeuforward.org/data-structure/longest-subarray-with-given-sum-k/
+// This is an optimal Solution, refer link or notes for other approaches.
 public class LongestSubArrayWithK {
 
     static void longestSubtArray(int[] nums, int k) {
         int n = nums.length;
-        int sum = 0;
-        int longestLength = 0;
-
-        for (int i = 0, j = 1; j < n; j++, i++) {
-            sum += (nums[i] + nums[j]);
-            if (sum == k) {
-                // +1 since i start at zero
-                longestLength = Math.max(longestLength, (j + (i + 1)));
-                sum = 0;
+        int sum = nums[0];
+        int maxLen = 0;
+        int left = 0, right = 0;
+        while (right < n) {
+            // if sum > k, reduce the subarray from left
+            // until sum becomes less or equal to k:
+            // left <= right mean there is a subarray
+            while (left <= right && sum > k) {
+                sum -= nums[left];
+                left++;
             }
+            if (sum == k)
+                maxLen = Math.max(maxLen, right - left + 1);
+
+            // move forward
+            right++;
+
+            // needed when right is at last element and left is still away from end.
+            if (right < n)
+                sum += nums[right];
         }
-        System.out.println(longestLength);
     }
 
     public static void main(String[] args) {
