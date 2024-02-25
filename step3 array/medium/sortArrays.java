dcae0153f5ff758f86c2e368649f06c553235771
Chetan-B-B <chetanchetu9740@gmail.com>
2024-02-25 18:30:40 +0530

    Dutch National flag algorithm

---
+++ step3 array/medium/sortArrays.java
@@ -0,0 +1,48 @@
+// https://takeuforward.org/data-structure/sort-an-array-of-0s-1s-and-2s/
+// Sort an array of 0s, 1s and 2s
+// Problem Statement: Given an array consisting of only 0s, 1s, and 2s. Write a program to in-place sort the array without using inbuilt sort functions. ( Expected: Single pass-O(N) and constant space)
+
+// Solved using This problem is a variation of the popular Dutch National flag algorithm
+import java.util.*;
+
+class Main {
+    public static void sortArray(ArrayList<Integer> arr, int n) {
+        int low = 0, mid = 0, high = n - 1; // 3 pointers
+
+        while (mid <= high) {
+            if (arr.get(mid) == 0) {
+                // swapping arr[low] and arr[mid]
+                int temp = arr.get(low);
+                arr.set(low, arr.get(mid));
+                arr.set(mid, temp);
+
+                low++;
+                mid++;
+
+            } else if (arr.get(mid) == 1) {
+                mid++;
+
+            } else {
+                // swapping arr[mid] and arr[high]
+                int temp = arr.get(mid);
+                arr.set(mid, arr.get(high));
+                arr.set(high, temp);
+
+                high--;
+            }
+        }
+    }
+
+    public static void main(String args[]) {
+        int n = 6;
+        ArrayList<Integer> arr = new ArrayList<>(Arrays.asList(new Integer[] { 0, 2, 1, 2, 0, 1 }));
+        sortArray(arr, n);
+        System.out.println("After sorting:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr.get(i) + " ");
+        }
+        System.out.println();
+
+    }
+
+}
