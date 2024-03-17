e5de1d5e7972d8366c4c5f85e3e6361094ed421d
Chetan-B-B <chetanchetu9740@gmail.com>
2024-03-18 01:38:51 +0530

    Brute force for permuation

--- step3 array/medium/nextPermutation.java
+++ step3 array/medium/nextPermutation.java
@@ -1,23 +1,63 @@
-// https://takeuforward.org/data-structure/next_permutation-find-next-lexicographically-greater-permutation/
+// Java program for the above approach 
 
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 
 public class nextPermutation {
-    public static void main(String[] args) {
-        bruteforce(new int[] { 2, 1, 3 });
+    // Function for swapping two numbers
+    static void swap(int nums[], int l, int i) {
+        int temp = nums[l];
+        nums[l] = nums[i];
+        nums[i] = temp;
     }
 
-    public static void bruteforce(int[] nums) {
-        // sort them in smallest to largest
-        int[] arr = nums.clone();
-        Arrays.sort(nums);
-        System.out.println(nums);
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = 0; j < nums.length; j++) {
-                if (i == j)
-                    break;
-                System.out.print(nums[i] + " " + nums[j] + " ");
+    // Function to find the possible
+    // permutations
+    static void permutations(ArrayList<int[]> res,
+            int[] nums, int l, int h) {
+        // Base case
+        // Add the array to result and return
+        if (l == h) {
+            res.add(Arrays.copyOf(nums, nums.length));
+            return;
+        }
+
+        // Permutations made
+        for (int i = l; i <= h; i++) {
+            // Swapping
+            swap(nums, l, i);
+
+            // Calling permutations for
+            // next greater value of l
+            permutations(res, nums, l + 1, h);
+
+            // Backtracking
+            swap(nums, l, i);
+        }
+    }
+
+    // Function to get the permutations
+    static ArrayList<int[]> permute(int[] nums) {
+        // Declaring result variable
+        ArrayList<int[]> res = new ArrayList<int[]>();
+        int x = nums.length - 1;
+
+        // Calling permutations for the first
+        // time by passing l
+        // as 0 and h = nums.size()-1
+        permutations(res, nums, 0, x);
+        return res;
+    }
+
+    // Driver Code
+    public static void main(String[] args) {
+        int[] nums = { 2, 2, 3 };
+        ArrayList<int[]> res = permute(nums);
+
+        // printing result
+        for (int[] x : res) {
+            for (int y : x) {
+                System.out.print(y + " ");
             }
             System.out.println();
         }
