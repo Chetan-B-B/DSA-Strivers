557808c0b981f4afa0298731a519a73d492d99d4
Chetan-B-B <chetanchetu9740@gmail.com>
2024-03-23 12:59:43 +0530

    did the better approach

--- step3 array/medium/SetMatrixZero.java
+++ step3 array/medium/SetMatrixZero.java
@@ -2,14 +2,18 @@ import java.util.Arrays;
 
 public class SetMatrixZero {
     public static void main(String[] args) {
+
         setMatrixWithZero(new int[][] { { 0, 1, 2, 0 }, { 3, 4, 5, 2 }, { 1, 3, 1, 5 } });
+        SetMatrixZeroBetter.setMatrixWithZeroBetter(new int[][] {
+                { 0, 1, 2, 0 },
+                { 3, 4, 5, 2 },
+                { 1, 3, 1, 5 } });
     }
 
     static void setMatrixWithZero(int[][] nums) {
         // brute forece
         int m = nums.length;// rows
         int n = nums[0].length; // colums These 2 piece of info provided int constarints section of leetcode.
-        System.out.println(m + " " + n);
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (nums[i][j] == 0) {
@@ -33,8 +37,8 @@ public class SetMatrixZero {
 
     static void updateRow(int idx, int col, int[][] nums) {
         for (int i = 0; i < col; i++) {
-            if (nums[idx][i] != 0) // need to avoid collision, if same row has 2 zero ,in first interation only we
-                                   // will make everthing -1 to avoid it we need the check
+            if (nums[idx][i] != 0) // need to avoid collision, if same row/col has 2 zero ,in first interation only
+                                   // we will make everthing -1 to avoid it we need the check
                 nums[idx][i] = -1;
         }
     }
@@ -44,5 +48,31 @@ public class SetMatrixZero {
             if (nums[j][idx] != 0) // need to avoid collision
                 nums[j][idx] = -1;
         }
+    }
+}
+
+class SetMatrixZeroBetter {
+    static void setMatrixWithZeroBetter(int[][] nums) {
+        int rows = nums.length;
+        int cols = nums[0].length;
+        int[] rowsBasket = new int[rows];
+        int[] colsBasket = new int[cols];
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (nums[i][j] == 0) {
+                    rowsBasket[i] = 1;
+                    colsBasket[j] = 1;
+                }
+            }
+        }
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (rowsBasket[i] == 1 || colsBasket[j] == 1) {
+                    nums[i][j] = 0;
+                }
+            }
+        }
+        System.out.println(Arrays.deepToString(nums));
     }
 }
