7cb3d582de4a59f78062f0f106d8408ea8d0511b
Chetan-B-B <chetanchetu9740@gmail.com>
2024-04-01 00:07:16 +0530

    completed

--- step3 array/medium/SpiralTraverseArray.java
+++ step3 array/medium/SpiralTraverseArray.java
@@ -4,7 +4,7 @@ public class SpiralTraverseArray {
                 { 1, 2, 3, 4 },
                 { 5, 6, 7, 8 },
                 { 9, 10, 11, 12 },
-                { 13, 14, 15, 16 }
+
         });
     }
 
@@ -24,6 +24,19 @@ public class SpiralTraverseArray {
                 System.out.println(nums[i][right]);
             right--;
 
+            if (top <= bottom) {
+                for (int i = right; i >= left; i--) {
+                    System.out.println(nums[bottom][i]);
+
+                }
+                bottom--;
+            }
+
+            if (left <= right) {
+                for (int i = bottom; i >= top; i--)
+                    System.out.println(nums[i][left]);
+                left++;
+            }
         }
 
     }
