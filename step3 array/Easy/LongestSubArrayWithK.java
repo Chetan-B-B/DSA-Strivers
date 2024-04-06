b9383ab0026d42219b778841c6ff6cda9ce6ddc8
Chetan-B-B <chetanchetu9740@gmail.com>
2024-04-06 21:53:37 +0530

    kicking back

--- step3 array/Easy/LongestSubArrayWithK.java
+++ step3 array/Easy/LongestSubArrayWithK.java
@@ -1,8 +1,12 @@
 // https://takeuforward.org/data-structure/longest-subarray-with-given-sum-k/
 // This is an optimal Solution, refer link or notes for other approaches.
+
+import java.util.HashMap;
+
 public class LongestSubArrayWithK {
 
     static void longestSubtArray(int[] nums, int k) {
+        // optimal for only +ive elements
         int n = nums.length;
         int sum = nums[0];
         int maxLen = 0;
@@ -25,9 +29,36 @@ public class LongestSubArrayWithK {
             if (right < n)
                 sum += nums[right];
         }
+        System.out.println(maxLen);
+
+    }
+
+    static void longestSubtArrayBetter(int[] nums, int k) {
+        // Optimal approach for both negative and pos elements
+        // Better approach if only have positives
+        int maxLen = 0, sum = 0;
+        HashMap<Integer, Integer> presumMap = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            if (sum == k)
+                maxLen = Math.max(maxLen, i + 1);
+
+            int rem = sum - k;
+            if (presumMap.containsKey(rem)) {
+                int len = i - presumMap.get(rem);
+                maxLen = Math.max(maxLen, len);
+            }
+
+            if (!presumMap.containsKey(rem)) {
+                presumMap.put(rem, i);
+            }
+        }
+        System.out.println(presumMap);
+        System.out.println(maxLen);
     }
 
     public static void main(String[] args) {
         longestSubtArray(new int[] { 2, 3, 5, 1, 9 }, 10);
+        longestSubtArrayBetter(new int[] { 2, 3, 5, 1, 9 }, 10);
     }
 }
