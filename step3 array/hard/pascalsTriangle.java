71abf9b032dbc2d09d8676a95fb574b1a8095382
Chetan-B-B <chetanchetu9740@gmail.com>
2024-05-07 23:18:32 +0530

    will come back soon

---
+++ step3 array/hard/pascalsTriangle.java
@@ -0,0 +1,9 @@
+public class pascalsTriangle {
+    static void pascalsTriangleV3(int n, int row, int col) {
+
+    }
+
+    public static void main(String[] args) {
+        pascalsTriangleV3(5, 5, 3);
+    }
+}
