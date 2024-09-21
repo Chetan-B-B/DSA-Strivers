f6eec5a6aaf63c4431de694116cd975a2668a1f1
Chetan-B-B <chetanchetu9740@gmail.com>
2024-09-21 19:54:44 +0530

    3 sum

--- design_guru/two_pointers/ThreesumORTripletSumtoZero.java
+++ design_guru/two_pointers/ThreesumORTripletSumtoZero.java
@@ -13,10 +13,32 @@ class TripletSumtoZero {
     static List<List<Integer>> threeSum(int[] arr) {
         List<List<Integer>> result = new ArrayList<>();
         Arrays.sort(arr);
+        int n = arr.length;
         for (int i = 0; i < arr.length; i++) {
             if (i == 0 && arr[i] == arr[i - 1]) {
                 continue;
             }
+            int j = i + 1;
+            int k = n - 1;
+            while (j < k) {
+                int sum = arr[i] + arr[j] + arr[k];
+                if (sum < 0) {
+                    j++;
+                } else if (sum > 0) {
+                    k--;
+                } else {
+                    result.add(Arrays.asList(arr[i], arr[j], arr[k]));
+                    j++;
+                    k--;
+
+                    // skip the duplicates
+                    while (j < k && arr[j] == arr[j - 1])
+                        j++;
+                    while (j < k && arr[k] == arr[k - 1])
+                        k--;
+                }
+            }
+
         }
         return result;
     }
